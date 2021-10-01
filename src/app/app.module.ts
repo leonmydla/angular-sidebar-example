5e35fe4af36491106cadb742955d156cb64a2dec
Leon Mydla <leon.mydla@accenture.com>
2021-10-01 16:20:11 +0200

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { SidebarToggleComponent } from './sidebar/sidebar-toggle/sidebar-toggle.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SidebarComponent,
+    SidebarToggleComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/sidebar/sidebar-toggle/sidebar-toggle.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-toggle',
+  templateUrl: './sidebar-toggle.component.html',
+  styleUrls: ['./sidebar-toggle.component.scss']
+})
+export class SidebarToggleComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
